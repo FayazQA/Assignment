7515b2e76923f421a4af3391e925c485891482a5
FayazQA <fayazbasha2442@gmail.com>
2025-05-26 20:03:39 +0530

    Assignments

---
+++ src/practiseSession/basicPrintStatment.java
@@ -0,0 +1,11 @@
+package practiseSession;
+
+public class basicPrintStatment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2025 New Start ");
+		System.out.println("Hello World");
+
+	}
+}
